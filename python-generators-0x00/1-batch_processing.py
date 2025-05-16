21cabc399a3095d1a4552100558a8a6235bae4d8
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-05-16 22:22:00 +0000

    Creating a new file

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,29 @@
+import seed
+
+connection = seed.connect_db()
+cursor = connection.cursor()
+
+def stream_users_in_batches(batch_size):
+    cursor.execute(f'SELECT * FROM users LIMIT {batch_size}')
+    rows = cursor.fetchall()
+    for row in rows:
+      yield row
+    # cursor.close()
+    # connection.close()
+
+for user in stream_users_in_batches(5):
+   print(user)
+
+   print('*' * 100)
+
+def batch_processing(batch_size):
+   for user in stream_users_in_batches(batch_size):
+      if user[3] > 25:
+         print(user)
+      else:
+         print('I am below 25')
+      
+batch_processing(3)
+
+cursor.close()
+connection.close()
