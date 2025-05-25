da35f79e44463bc9a3d5052d90b6d2face5a2764
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-05-25 15:59:14 +0000

    Update 3-concurrent.py

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -5,3 +5,7 @@ async_fetch_users()
 async_fetch_older_users()
 asyncio.gather()
 asyncio.run(fetch_concurrently())
+
+import aiosqlite", "connect", "execute
+async def async_fetch_users()", "async def async_fetch_older_users()", "return", "await
+await asyncio.gather
